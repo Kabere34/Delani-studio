fd0e54388bd4506140247adc707966159e8cf08c
Ivy Kabere <kabereivy@gmail.com>
2022-03-12 18:39:28 +0300

    add functions to toggle over the what-we-do section

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#services").click(function () {
+    $(".services-text").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#development").click(function () {
+    $(".development-text").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#product").click(function () {
+    $(".product-text").toggle();
+  });
+});
