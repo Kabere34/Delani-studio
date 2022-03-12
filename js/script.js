900cda303af9fe68bd55cc7564d46337d339a256
Ivy Kabere <kabereivy@gmail.com>
2022-03-12 20:40:45 +0300

    fx:add icons to what we do section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
-$(document).ready(function () {
-  $("#services").click(function () {
-    $(".services-text").toggle();
-  });
-});
-$(document).ready(function () {
-  $("#development").click(function () {
-    $(".development-text").toggle();
-  });
-});
-$(document).ready(function () {
-  $("#product").click(function () {
-    $(".product-text").toggle();
-  });
-});
+// $(document).ready(function () {
+//   $("#services").click(function () {
+//     $("#services").hide(0, function () {
+//       $(".services-text").show(0);
+//     });
+//   });
+// });
+// $(document).ready(function () {
+//   $(".services-text").click(function () {
+//     $(".services-text").hide(0, function () {
+//       $("#services").show(0);
+//     });
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#development").click(function () {
+//     $("#development").hide(0, function () {
+//       $(".development-text").show(0);
+//     });
+//   });
+// });
+// $(document).ready(function () {
+//   $(".development-text").click(function () {
+//     $(".development-text").hide(0, function () {
+//       $("#development").show(0);
+//     });
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#product").click(function () {
+//     $("#product").hide(0, function () {
+//       $(".product-text").show(0);
+//     });
+//   });
+// });
+// $(document).ready(function () {
+//   $(".product-text").click(function () {
+//     $(".product-text").hide(0, function () {
+//       $("#product").show(0);
+//     });
+//   });
+// });
